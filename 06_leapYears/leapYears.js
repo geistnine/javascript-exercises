c50cbeecfb1160dbd5986c9b0ca644020e2bab09
Steve <sgeiger5516@gmail.com>
2022-06-29 15:28:31 -0400

    Completed 06 and 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are divisible by four but are not leap years
+    // if divisible by 100, but 400 is ok
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
+// num.toFixed(decimal) rounds to desired decimal, returns
+// a string that will need to be converted back to num
+
+const ftoc = function(degreesF) {
+  // c = (f - 32) * (5 / 9)
+  const degreesC = (degreesF - 32) * (5 / 9);
+  return parseFloat(degreesC.toFixed(1));
 
 };
 
-const ctof = function() {
-
+const ctof = function(degreesC) {
+  // f = (c * (9/5) + 32)
+  const degreesF = (degreesC * (9 / 5 ) + 32)
+  return parseFloat(degreesF.toFixed(1));
 };
+
+console.log(ftoc(100));
 
 // Do not edit below this line
 module.exports = {
